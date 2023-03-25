5deea5b2a174633a374667ecd70e3437f3dadc2b
Räghib Hasan <raghibmidhat@gmail.com>
2023-03-25 19:23:12 +0200

    feat(lib): add react-testing-library

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,11 @@
-/**
- * @format
- */
+import { render, screen } from '@testing-library/react-native';
 
 import 'react-native';
 import React from 'react';
 import App from '../src/App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
 it('renders correctly', () => {
-  renderer.create(<App />);
+  render(<App />);
+
+  expect(screen.getByText(/Hello world/g)).toBeDefined();
 });
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('jest').Config} */
+
+const config = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  verbose: true,
+};
+
+module.exports = config;
