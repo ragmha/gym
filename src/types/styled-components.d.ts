2fc0091eeeb2177c70e2ce5f11251c65befd3157
Räghib Hasan <raghibmidhat@gmail.com>
2023-04-02 13:04:28 +0300

    fix(theme): remove zod and use correct types

---
+++ src/types/styled-components.d.ts
@@ -0,0 +1,6 @@
+import type { Theme } from '@/theme'
+
+declare module 'styled-components/native' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
+}
