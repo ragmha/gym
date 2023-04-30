96f9c61267c9735166a1af465d732c83989bbff9
Räghib Hasan <raghibmidhat@gmail.com>
2023-04-30 08:05:19 +0300

    chore(HomeScreen): remove tailwind code

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,23 +1,9 @@
-import {
-  View,
-  Text,
-  SafeAreaView,
-  StatusBar,
-  useColorScheme,
-} from 'react-native'
+import { View, Text } from 'react-native'
 
 export const HomeScreen = () => {
-  const isDarkMode = useColorScheme() === 'dark'
-  const backgroundStyle = 'bg-neutral-300 dark:bg-slate-900'
-
   return (
-    <View className="content-center">
-      <SafeAreaView className={backgroundStyle}>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <View className="bg-white dark:bg-black">
-          <Text>Hello world</Text>
-        </View>
-      </SafeAreaView>
+    <View>
+      <Text>Hello world</Text>
     </View>
   )
 }
