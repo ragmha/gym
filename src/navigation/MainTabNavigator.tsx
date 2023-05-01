33eb4e12c71f28bcede7e86e4b6faf1f8fef7359
Räghib Hasan <raghibmidhat@gmail.com>
2023-05-01 07:47:55 +0300

    feat(screen): add a theme item to settings screen

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -1,6 +1,7 @@
-import type { MainTabParamList } from './types'
+import type { MainTabParamList } from '@/navigation/types'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
 import { HomeScreen } from '../screens/HomeScreen'
 
@@ -11,7 +12,15 @@ const Tab = createBottomTabNavigator<MainTabParamList>()
 const MainTabNavigator: React.FC = () => (
   <Tab.Navigator>
     <Tab.Screen name="Home" component={HomeScreen} />
-    <Tab.Screen name="Settings" component={SettingsScreen} />
+    <Tab.Screen
+      name="Settings"
+      component={SettingsScreen}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <FontAwesome5 name="comments" color={color} size={20} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 )
 
