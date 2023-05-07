07e9393c3957b1660d31414db62b31448dfab3ef
Räghib Hasan <raghibmidhat@gmail.com>
2023-05-07 10:29:28 +0300

    feat(exeriseScreen): initialize exercise screen

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -2,13 +2,15 @@ import type { StackParamList } from './types'
 
 import { createStackNavigator } from '@react-navigation/stack'
 
-import { DashboardScreen } from '../screens/DashboardScreen'
+import { DashboardScreen } from '@/screens/DashboardScreen'
+import { ExercisesScreen } from '@/screens/ExercisesScreen'
 
 const Stack = createStackNavigator<StackParamList>()
 
 const StackNavigator: React.FC = () => (
   <Stack.Navigator>
     <Stack.Screen name="Home" component={DashboardScreen} />
+    <Stack.Screen name="Exercises" component={ExercisesScreen} />
   </Stack.Navigator>
 )
 
---
+++ src/screens/ExercisesScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export const ExercisesScreen = () => {
+  return (
+    <View>
+      <Text>Exercises Screen</Text>
+    </View>
+  )
+}
