81c1eb94da84bea6040c38e0c96029b69058b874
Räghib Hasan <raghibmidhat@gmail.com>
2023-05-07 15:56:26 +0300

    fix(MainNavigator): update correct colors

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -5,30 +5,36 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import MainTabNavigator from './MainTabNavigator'
 
+import { useCurrentTheme } from '@/hooks/use-current-theme'
+
 const Stack = createStackNavigator<RootStackPramList>()
 
-const RootNavigator: React.FC = () => (
-  <NavigationContainer
-    theme={{
-      colors: {
-        primary: 'rgb(255, 45, 85)',
-        background: '#333333',
-        card: '#333333',
-        text: '#FFFFFF',
-        border: '#000000',
-        notification: 'rgb(255, 69, 58)',
-      },
-      dark: true,
-    }}
-  >
-    <Stack.Navigator>
-      <Stack.Screen
-        name="MainTabNavigator"
-        component={MainTabNavigator}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
-)
+const RootNavigator: React.FC = () => {
+  const theme = useCurrentTheme()
+
+  return (
+    <NavigationContainer
+      theme={{
+        colors: {
+          primary: theme.colors.secondary,
+          background: theme.colors.surface,
+          card: theme.colors.surface,
+          text: theme.colors.onSurface,
+          border: theme.colors.background,
+          notification: theme.colors.error,
+        },
+        dark: true,
+      }}
+    >
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MainTabNavigator"
+          component={MainTabNavigator}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
 export default RootNavigator
