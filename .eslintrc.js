af0a1dbaa8dd010654af07638ebe7561eb994a2c
Räghib Hasan <raghibmidhat@gmail.com>
2024-11-17 17:12:49 +0200

    update deps to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
-module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['prettier', 'unused-imports'],
-  rules: {
-    'prettier/prettier': 'error',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': [
-      'warn',
-      {
-        enableDangerousAutofixThisMayCauseInfiniteLoops: true,
-      },
-    ],
+export default [
+  {
+    extends: ['expo', 'prettier'],
+    plugins: ['prettier', 'unused-imports'],
+    rules: {
+      'prettier/prettier': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': [
+        'warn',
+        {
+          enableDangerousAutofixThisMayCauseInfiniteLoops: true,
+        },
+      ],
+    },
   },
-}
+]
