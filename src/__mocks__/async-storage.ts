489aa43de05be8629eaef8100db647c492c82b43
Räghib Hasan <raghibmidhat@gmail.com>
2025-02-26 18:50:13 +0200

    add test

--- src/__mocks__/async-storage.ts
+++
@@ -1,8 +0,0 @@
-const mockAsyncStorage = {
-  getItem: async (key: string) => null,
-  setItem: async (key: string, value: string) => undefined,
-  removeItem: async (key: string) => undefined,
-  clear: async () => undefined,
-}
-
-export default mockAsyncStorage
